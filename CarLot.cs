8a73f8b49b07f68487ef4223b03159049fe5c28b
Drew Myers <drew.myers59@gmail.com>
2022-08-31 22:12:14 -0400

    initial commit

---
+++ CarLot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    internal class CarLot
+    {
+        public CarLot()
+        {
+        }
+        
+        public List<Car> Cars = new List<Car>();
+      //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarLotSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lot = new CarLot();
+
+            Car carOne = new Car();
+
+            carOne.Year = 2014;
+            carOne.Make = "Honda";
+            carOne.Model = "Ridge Line";
+            carOne.EngineNoise = "No";
+            carOne.HonkNoise = "Beep";
+            carOne.IsDriveable = true;
+
+            lot.Cars.Add(carOne);
+
+            var carTwo = new Car()
+            {
+                Year = 1998,
+                Make = "Toyota",
+                Model = "Camry",
+                EngineNoise = "...",
+                HonkNoise = "Toot",
+                IsDriveable = false
+            };
+
+            lot.Cars.Add(carTwo);
+
+            var carThree = new Car(2022, "Nissan", "Pathfinder", "Vroom", "Honk", true);
+
+            lot.Cars.Add(carThree);
+
+            carOne.MakeEngineNoise(carOne.EngineNoise);
+            carOne.MakeHonkNoise(carOne.HonkNoise);
+            carTwo.MakeEngineNoise(carTwo.EngineNoise);
+            carTwo.MakeHonkNoise(carTwo.HonkNoise);
+            carThree.MakeEngineNoise(carThree.EngineNoise);
+            carThree.MakeHonkNoise(carThree.HonkNoise);
+
+            foreach (var car in lot.Cars)
+            {
+                Console.WriteLine($"{car.Year} {car.Make} {car.Model}");
+            }
+        }
+    }
+}
